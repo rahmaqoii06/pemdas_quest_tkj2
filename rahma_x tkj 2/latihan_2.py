a8d65ff3de495c8ae40709fc28074bfbf69ac02c
rahmaqoii06 <111480116+rahmaqoii06@users.noreply.github.com>
2023-02-13 09:51:41 +0700

    Add files via upload

---
+++ rahma_x tkj 2/latihan_2.py
@@ -0,0 +1,13 @@
+'''
+Buatlah program menghitung luas bangun datar menggunakan fungsi. Gunakan parameter
+- segitiga
+- persegi panjang
+'''
+
+def luas_segitiga():
+    print("L : 1/2 x a x t")
+    print("nilai", a)
+    print("nilai", t)
+
+luas_segitiga("L : 1/2 x 16 x 21")
+
---
+++ rahma_x tkj 2/latihan_fungsi.py
@@ -0,0 +1,42 @@
+'''fungsi pada python didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("Assalamu'alaikum !! Akhi Ukhty")
+
+salam()
+salam()
+salam()
+
+'''buatlah sebuah fungsi versi kalian'''
+
+def sapa():
+    print("hai apakabar?")
+
+sapa()
+sapa()
+sapa()
+
+'''Fungsi dengan parameter'''
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+    print("Halo perkenalkan nama saya", nama)
+
+say_hi("Indi")
+say_hi("Icha")
+
+'''Buatlah fungsi dengan 3 parameter'''
+
+def perkenalan(nama, umur, sekola):
+    # Metode
+    print("nama saya:", nama)
+    print("umur saya:", umur, 'tahun')
+    print("sekola saya:", sekola)
+
+perkenalan("saputra", 18, "harapanbangsa")
+
+print("\n\n============= BATAS ===============\n\n")
+
+perkenalan("indiansyah", 18, "harapanbangsa")
